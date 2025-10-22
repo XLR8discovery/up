8cbbe9d21d75e46a37189015000f4b87a1a53dc6
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2025-10-22 07:35:25 -0400

    Add files via upload

---
+++ debug.go
@@ -0,0 +1,94 @@
+// Copyright (C) 2025 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package container
+
+import (
+	"github.com/compose-spec/compose-go/types"
+	"github.com/spf13/cobra"
+	"github.com/zeebo/errs/v2"
+
+	"xlr8d.io/oss-up/cmd"
+	"xlr8d.io/oss-up/pkg/common"
+	"xlr8d.io/oss-up/pkg/recipe"
+	"xlr8d.io/oss-up/pkg/runtime/compose"
+	"xlr8d.io/oss-up/pkg/runtime/runtime"
+)
+
+func enableDebugCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "enable <selector>...",
+		Short: "turn on local debugging with Delve (go debugger)",
+		Long:  "Add environment variable which will activate the DLV debug. Container won't start until the agent is connected. " + cmd.SelectorHelp,
+		Args:  cobra.MinimumNArgs(1),
+		RunE:  cmd.ExecuteOSSUP(enableDebug),
+	}
+}
+
+func disableDebugCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "disable <selector>...",
+		Short: "turn off local debugging with DLV",
+		Args:  cobra.MinimumNArgs(1),
+		RunE:  cmd.ExecuteOSSUP(disableDebug),
+	}
+}
+
+func init() {
+	debugCmd := cobra.Command{
+		Use:   "debug",
+		Args:  cobra.NoArgs,
+		Short: "enable/disable local DLV based go debug",
+	}
+
+	debugCmd.AddCommand(enableDebugCmd())
+	debugCmd.AddCommand(disableDebugCmd())
+	cmd.RootCmd.AddCommand(&debugCmd)
+}
+
+func enableDebug(st recipe.Stack, rt runtime.Runtime, selector []string) error {
+	return runtime.ModifyService(st, rt, selector, func(s runtime.Service) error {
+		composeService, ok := s.(*compose.Service)
+		if !ok {
+			return errs.Errorf("this subcommand is supported only for compose based environments")
+		}
+		return composeService.TransformRaw(func(composeService *types.ServiceConfig) error {
+			tr := "true"
+			composeService.Environment["GO_DLV"] = &tr
+			for _, portConfig := range composeService.Ports {
+				if portConfig.Mode == "ingress" &&
+					portConfig.Target == 2345 &&
+					portConfig.Published == 2345 &&
+					portConfig.Protocol == "tcp" {
+					return nil
+				}
+			}
+			composeService.Ports = append(composeService.Ports, types.ServicePortConfig{
+				Mode:      "ingress",
+				Target:    2345,
+				Published: 2345,
+				Protocol:  "tcp",
+			})
+			return nil
+		})
+	})
+}
+
+func disableDebug(st recipe.Stack, rt runtime.Runtime, args []string) error {
+	selector, _ := common.SplitArgsSelector1(args) // TODO: doesn't look right
+	return runtime.ModifyService(st, rt, selector, func(s runtime.Service) error {
+		composeService, ok := s.(*compose.Service)
+		if !ok {
+			return errs.Errorf("this subcommand is supported only for compose based environments")
+		}
+		return composeService.TransformRaw(func(composeService *types.ServiceConfig) error {
+			delete(composeService.Environment, "GO_DLV")
+			for i, port := range composeService.Ports {
+				if port.Target == 2345 {
+					composeService.Ports = append(composeService.Ports[:i], composeService.Ports[i+1:]...)
+				}
+			}
+			return nil
+		})
+	})
+}
