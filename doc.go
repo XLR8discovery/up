158693617e7759d41f99e9b2049eac12ffe464a8
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2025-10-23 07:50:17 -0400

    Add files via upload

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Copyright (C) 2025 XLR8discovery PBC
+// See LICENSE for copying information.
+
+// Package container package contains commands which are valid only for local container based environment.
+package container
