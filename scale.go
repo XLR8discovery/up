b3dcaf426367af4b5d6a63560e34f51046a5ec87
XLR8discovery <34426105+XLR8discovery@users.noreply.github.com>
2025-10-27 08:15:01 -0400

    Add files via upload

---
+++ scale.go
@@ -0,0 +1,53 @@
+// Copyright (C) 2025 XLR8discovery PBC
+// See LICENSE for copying information.
+
+package container
+
+import (
+	"strconv"
+
+	"github.com/compose-spec/compose-go/types"
+	"github.com/spf13/cobra"
+	"github.com/zeebo/errs/v2"
+
+	"xlr8d.io/oss-up/cmd"
+	"xlr8d.io/oss-up/pkg/common"
+	"xlr8d.io/oss-up/pkg/recipe"
+	"xlr8d.io/oss-up/pkg/runtime/runtime"
+)
+
+func scaleCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "scale <selector>... <number>",
+		Short: "static scale of services",
+		Args:  cobra.MinimumNArgs(2),
+		Long: "This command creates multiple instances of the service or services. After this scale services couldn't be scaled up with `docker compose scale` any more. " +
+			"But also not required to scale up and down and it's possible to do per instance local bindmount",
+		RunE: cmd.ExecuteOSSUP(func(st recipe.Stack, rt runtime.Runtime, args []string) error {
+			selector, number := common.SplitArgsSelector1(args)
+			_ = number
+			return cmd.ChangeCompose(st, rt, selector, func(composeService *types.ServiceConfig) error {
+				return scale(composeService, args[0]) // TODO: doesn't look right
+			})
+		}),
+	}
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(scaleCmd())
+}
+
+func scale(composeService *types.ServiceConfig, scale string) error {
+	instances, err := strconv.ParseUint(scale, 10, 64)
+	if err != nil {
+		return errs.Wrap(err)
+	}
+	if instances == 1 {
+		composeService.Deploy = nil
+	} else if composeService.Deploy == nil {
+		composeService.Deploy = &types.DeployConfig{Replicas: &instances}
+	} else {
+		*composeService.Deploy.Replicas = instances
+	}
+	return nil
+}
